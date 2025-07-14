a11d128452278f59a746a436b7d091101fa61f77
Lucaz Lindgren <lucazzlindgren@hotmail.com>
2025-07-15 00:18:26 +0200

    update flake

--- flake.nix
+++ flake.nix
@@ -13,20 +13,24 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        runtime_libs = with pkgs; [
+          raylib
+          wayland
+          libxkbcommon
+          libglvnd
+          mesa
+          xorg.libX11
+          xorg.libXi
+          xorg.libXcursor
+          xorg.libXrandr
+          xorg.libXinerama
+        ];
       in
       with pkgs;
       {
         devShells.default = mkShell {
-          nativeBuildInputs = [
-            raylib
-            xorg.libX11
-            xorg.libXrandr
-            xorg.libXinerama
-            xorg.libXcursor
-            xorg.libXi
-          ];
-
-          buildInputs = with pkgs; [
+          buildInputs = runtime_libs;
+          nativeBuildInputs = with pkgs; [
             odin
             ols # provides both ols and odinfmt
             python314
@@ -36,7 +40,10 @@
             git
             cc65 # C compiler for 6502 processor family
             glsl_analyzer
+
           ];
+
+          # LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath runtime_libs;
 
           shellHook = ''
             echo "Entering Nemu dev shell"
